081041d26555265069a485b6a7912f0b3d0d1919
Fu Li <tamufuli@gmail.com>
2017-06-30 12:19:55 -0500

    add second fig in table2

--- project_stage_3.py
+++ project_stage_3.py
@@ -93,22 +93,57 @@ class MyTableWidget(QWidget):
         self.tab2_layout = QHBoxLayout(self)             # create a Layout, which will be setted for tab_2
         
         self.tab2_layout_R = QVBoxLayout(self)
-        self.figure = plt.figure()
-        #self.figure, self.ax = plt.subplots()                       # a figure instance to plot on
+        self.figure_R = plt.figure()
+        #self.figure_R, self.ax_R = plt.subplots()                       # a figure instance to plot on
         # this is the Canvas Widget that displays the `figure`, it takes the `figure` instance as a parameter to __init__
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self) # this is the Navigation widget, it takes the Canvas widget and a parent
-        self.button_plot = QPushButton('Plot')           # Just some button connected to `plot` method
-        self.button_plot.clicked.connect(self.plot)      
-        self.tab2_layout_R.addWidget(self.toolbar)         # this also needed to show the Navigation of plot
-        self.tab2_layout_R.addWidget(self.canvas)          # add Canvas Widget(plot widget) onto tab_2
-        self.tab2_layout_R.addWidget(self.button_plot)
+        self.canvas_R = FigureCanvas(self.figure_R)
+        self.toolbar_R = NavigationToolbar(self.canvas_R, self) # this is the Navigation widget, it takes the Canvas widget and a parent
+        self.button_plot_R = QPushButton('Plot')           # Just some button connected to `plot` method
+        self.button_plot_R.clicked.connect(self.plot_R)      
+        self.tab2_layout_R.addWidget(self.toolbar_R)         # this also needed to show the Navigation of plot
+        self.tab2_layout_R.addWidget(self.canvas_R)          # add Canvas Widget(plot widget) onto tab_2
+        self.tab2_layout_R.addWidget(self.button_plot_R)
 
+        self.tab2_layout_L = QVBoxLayout(self)
+        self.figure_L = plt.figure()
+        #self.figure_R, self.ax_R = plt.subplots()                       # a figure instance to plot on
+        # this is the Canvas Widget that displays the `figure`, it takes the `figure` instance as a parameter to __init__
+        self.canvas_L = FigureCanvas(self.figure_L)
+        self.toolbar_L = NavigationToolbar(self.canvas_L, self) # this is the Navigation widget, it takes the Canvas widget and a parent
+        self.button_plot_L = QPushButton('Plot')           # Just some button connected to `plot` method
+        self.button_plot_L.clicked.connect(self.plot_L)      
+        self.tab2_layout_L.addWidget(self.toolbar_L)         # this also needed to show the Navigation of plot
+        self.tab2_layout_L.addWidget(self.canvas_L)          # add Canvas Widget(plot widget) onto tab_2
+        self.tab2_layout_L.addWidget(self.button_plot_L)
+
+
+
+        self.tab2_layout.addLayout(self.tab2_layout_L)
         self.tab2_layout.addStretch()                      # put the plot_layout right side
         self.tab2_layout.addLayout(self.tab2_layout_R)
         ############################################################################################
 
         self.tab2.setLayout(self.tab2_layout)            # set tab2.layout to be the layout of tab_2       
+
+
+    def plot_L(self):
+        ''' plot some random stuff '''
+        # random data
+        data = [random.random() for i in range(10)]
+
+        # instead of ax.hold(False)
+        self.figure_L.clear()
+
+        # create an axis
+        ax_L = self.figure_L.add_subplot(111)
+
+        # plot data
+        ax_L.plot(data, '*-')
+
+        # refresh canvas
+        self.canvas_L.draw()
+
+        
 
     def getInteger(self):
         i, okPressed = QInputDialog.getInt(self, "Get integer","Percentage:", 28, 0, 100, 1)
@@ -118,40 +153,47 @@ class MyTableWidget(QWidget):
             print(i)
             print("amp = ", self.amp)
     
-    def plot(self):
+    def plot_R(self):
         ''' plot some random stuff '''
-        self.ax = self.figure.add_subplot(111)
-        self.line, = plt.plot([], [], animated=True)
-        self.xdata, self.ydata = [], []
-        self.line.set_data(self.xdata, self.ydata) 
-        self.ax.set_ylim(-2, 2)
-        self.ax.set_xlim(0, 10)
+        self.ax_R = self.figure_R.add_subplot(111)
+
+        self.line_R, = self.ax_R.plot([], [], animated=True)  
+        #import to use "self.ax_R.plot" not "plt.plot" that will mix two figures
         
-        ani = animation.FuncAnimation(self.figure, self.update, self.data_gen, blit=True, interval=1,
+        self.xdata_R, self.ydata_R = [], []
+        self.line_R.set_data(self.xdata_R, self.ydata_R) 
+        self.ax_R.set_ylim(-2, 2)
+        self.ax_R.set_xlim(0, 10)
+        
+        ani = animation.FuncAnimation(self.figure_R, self.update, self.data_gen, blit=True, interval=1,
                               repeat=True)
-        #self.figure.show()
+        #self.figure_R.show()
         # refresh canvas
-        self.ax.figure.canvas.draw()
-        #self.canvas.draw()
-        #plt.show()
+        #self.figure_R.canvas.draw()
+        self.ax_R.figure.canvas.draw()
+
 
     def update(self, data):
         x, y = data
-        self.xdata.append(x)
-        self.ydata.append(y)
-        xmin, xmax = self.ax.get_xlim()
-        print("x = ", x,"        ", "y =" , y)
+        self.xdata_R.append(x)
+        self.ydata_R.append(y)
+        xmin, xmax = self.ax_R.get_xlim()
+        #print("x = ", x,"        ", "y =" , y)
         if x >= xmax:
-            self.ax.set_xlim(0, 2*xmax)
-            self.ax.figure.canvas.draw()
-            #self.canvas.draw()
+            self.ax_R.set_xlim(0, 2*xmax)
+            self.ax_R.figure.canvas.draw()
+            #self.canvas_R.draw()
             print("figure_axes changed")
 
-        self.line.set_data(self.xdata, self.ydata)
-        return self.line,
+        self.line_R.set_data(self.xdata_R, self.ydata_R)
+        return self.line_R,
 
     def data_gen(self, t=0):
         cnt = 0
+        while cnt < 1000:
+            cnt += 1
+            t += 0.1
+            yield t, np.sin(t)
 '''
         with nidaqmx.Task() as task:
             task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
